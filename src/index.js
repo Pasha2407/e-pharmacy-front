353edeb371203de1ab66bd2ccecf3c123af0e5e0
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-02-20 15:03:58 +0100

    fix path

--- src/index.js
+++ src/index.js
@@ -8,21 +8,25 @@ import { store, persistor } from './redux/store'
 import { App } from './App';
 import './index.scss';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '*',
+      element: <App />,
+    },
+  ],
   {
-    path: '*',
-    element: <App />,
-  },
-], {
-  future: {
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true
+    basename: '/e-pharmacy-front',
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
   }
-});
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
