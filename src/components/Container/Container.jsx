118e1300efdde6eca8c957024bb4a341477387df
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-02-20 14:40:33 +0100

    Updates

--- src/components/Container/Container.jsx
+++
@@ -1,5 +0,0 @@
-import s from './Container.module.scss';
-
-export const Container = ({ children }) => {
-  return <div className={s.container}>{children}</div>;
-};
--- src/redux/auth/authSelectors.js
+++
@@ -1,4 +0,0 @@
-export const selectUserData = state => state.auth.userData;
-export const selectAuthenticated = state => state.auth.authenticated;
-export const selectAuthIsLoading = state => state.auth.isLoading;
-export const selectAuthError = state => state.auth.error;
